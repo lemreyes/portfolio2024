80b72a119db3b4a9ee85b17ed6d43d5431f347aa
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-24 22:32:22 +0800

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-6xl font-bold text-gray-700">Hello World</h1>
     </>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className="flex flex-row gap-x-8 font-medium text-[20px]">
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
