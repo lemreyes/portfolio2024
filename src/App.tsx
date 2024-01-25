bfa1ef5ce3e9a3af160a1b2f402929b645cf7b6c
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-25 22:35:30 +0800

    hero text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-6xl font-bold text-gray-700 playfair">Hello World</h1>
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <section id="Hero" className="flex flex-row">
+      <div className="w-1/2 flex flex-col items-start">
+        <span className="uppercase nunito text-[20px] font-bold">
+          FULL STACK DEVELOPER
+        </span>
+        <h1 className="playfair text-6xl font-bold leading-[120%] text-left">
+          Hello, my name is Lemuel Leogene Reyes
+        </h1>
+        <p className="nunito text-2xl leading-[150%] text-left text-gray-500">
+          Seasoned software engineer and engineering manager with 17 years of
+          expertise, ready to elevate your projects as a full stack web
+          developer.
+        </p>
+      </div>
+      <div className="w-1/2"></div>
+    </section>
+  );
+}
