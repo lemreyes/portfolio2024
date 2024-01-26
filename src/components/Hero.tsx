51c03a7ccb4a9a025de11de3d7b152ec5bb4919b
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-26 23:31:39 +0800

    hero section colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import profile_image from "../assets/_MG_0979_reverse_web.jpg";
+
 export default function Hero() {
   return (
-    <section id="Hero" className="flex flex-row">
-      <div className="w-1/2 flex flex-col items-start">
-        <span className="uppercase nunito text-[20px] font-bold">
+    <section id="Hero" className="flex flex-row gap-x-16">
+      <div className="w-2/3 flex flex-col items-start">
+        <span className="uppercase nunito text-[20px] font-bold text-primary">
           FULL STACK DEVELOPER
         </span>
         <h1 className="playfair text-6xl font-bold leading-[120%] text-left">
@@ -14,7 +16,7 @@ export default function Hero() {
           Developer.
         </p>
         <div>
-          <button className="px-4 py-1 roboto text-lg font-medium leading-[150%] mr-4">
+          <button className="px-4 py-1 roboto text-lg text-white font-medium leading-[150%] mr-4 bg-primary rounded-xl border-2 border-primary">
             Projects
           </button>
           <button className="px-4 py-1 roboto text-lg font-medium leading-[150%] border-2 rounded-xl border-black">
@@ -22,7 +24,9 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="w-1/2"></div>
+      <div className="w-1/3 px-16 py-16 bg-primary items-center justify-center">
+        <img src={profile_image} alt="profile picture" />
+      </div>
     </section>
   );
 }
