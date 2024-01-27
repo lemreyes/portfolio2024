65957f4027aeb0cc1bd91279eb04bbdf8a9d18e3
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-27 19:53:10 +0800

    add Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <section id="Projects" className="mt-16 flex flex-col items-center">
+      <div className="flex flex-col items-center">
+        <h2 className="playfair text-5xl font-bold leading-[72px]">Projects</h2>
+        <div className="h-1 bg-primary w-36">&nbsp;</div>
+      </div>
+    </section>
+  );
+}
