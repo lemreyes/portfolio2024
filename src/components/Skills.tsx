90fcc614dfdf1f0aa7a3106fc8859deafa8b9c25
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-01-27 10:31:32 +0800

    skills heading accent

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,11 @@
 export default function Skills() {
   return (
     <section id="Skills" className="mt-8">
-      <h2 className="playfair text-5xl font-bold leading-[72px]">Skills</h2>
+      <div className="flex flex-col items-center">
+        <h2 className="playfair text-5xl font-bold leading-[72px]">Skills</h2>
+        <div className="h-1 bg-primary -mt-2 w-24">&nbsp;</div>
+      </div>
+
       <ul>
         <li>Javascript</li>
         <li>Typescript</li>
