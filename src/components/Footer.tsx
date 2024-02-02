ebe25cf4abd921fa277f590633670b2d3f46329c
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-02-02 23:07:19 +0800

    footer mobile layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import linkedin_icon from "../assets/linkedin_icon.svg";
 
 export default function Footer() {
   return (
-    <footer className="mt-32">
+    <footer className="mt-24 tablet:mt-32 mb-16 flex flex-col items-center">
       <div className="flex flex-row gap-4 items-center justify-center mb-8">
         <img src={github_icon} width={30} alt="github icon" />
         <img src={linkedin_icon} width={30} alt="linkedin icon" />
       </div>
-      <span className="nunito text-[16px] text-regularText">
+      <span className="nunito text-[16px] text-regularText text-center">
         Lemuel Leogene Reyes 2024
       </span>
     </footer>
