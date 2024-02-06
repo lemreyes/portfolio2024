1b68b5c25640fac30ff89fbbae7de026279d9ee1
lemreyes <67998813+lemreyes@users.noreply.github.com>
2024-02-06 22:58:24 +0800

    footer icon hover states

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,38 @@
 import github_icon from "../assets/github-mark.svg";
 import linkedin_icon from "../assets/linkedin_icon.svg";
+import github_icon_hover from "../assets/github-mark_hover.svg";
+import linkedin_icon_hover from "../assets/linkedin_icon_hover.svg";
+import { useState } from "react";
 
 export default function Footer() {
+  const [isHoverGithubIcon, setIsHoverGithubIcon] = useState(false);
+  const [isHoverLinkedinIcon, setIsHoverLinkedinIcon] = useState(false);
+
+  const toggleGithubIcon = () => {
+    setIsHoverGithubIcon(!isHoverGithubIcon);
+  };
+
+  const toggleLinkedinIcon = () => {
+    setIsHoverLinkedinIcon(!isHoverLinkedinIcon);
+  };
+
   return (
     <footer className="mt-24 tablet:mt-32 mb-16 flex flex-col items-center">
       <div className="flex flex-row gap-4 items-center justify-center mb-8">
-        <img src={github_icon} width={30} alt="github icon" />
-        <img src={linkedin_icon} width={30} alt="linkedin icon" />
+        <img
+          src={isHoverGithubIcon ? github_icon_hover : github_icon}
+          width={50}
+          alt="github icon"
+          onMouseEnter={toggleGithubIcon}
+          onMouseLeave={toggleGithubIcon}
+        />
+        <img
+          src={isHoverLinkedinIcon ? linkedin_icon_hover : linkedin_icon}
+          width={50}
+          alt="linkedin icon"
+          onMouseEnter={toggleLinkedinIcon}
+          onMouseLeave={toggleLinkedinIcon}
+        />
       </div>
       <span className="nunito text-[16px] text-regularText text-center">
         Lemuel Leogene Reyes 2024
